2af919a6db9004ee3c93dc9996c1edd3e1ca2ca6
rvyhvn <hanshitrash00@gmail.com>
2023-05-10 16:34:07 +0800

    Edit: User.java setter/getter

--- app/src/test/java/lasilu/User.java
+++ app/src/test/java/lasilu/User.java
@@ -20,4 +20,52 @@ public class User {
     this.email = email;
     this.password = password;
   }
+
+  public int getId(){
+    return id;
+  }
+
+  public void setId(int id){
+    this.id = id;
+  }
+
+  public String getUsername(){
+    return username;
+  }
+
+  public void setUsername(String username){
+    this.username = username;
+  }
+
+  public String getNama(){
+    return nama;
+  }
+
+  public void setNama(String nama){
+    this.nama = nama;
+  }
+
+  public char getGender(){
+    return gender;
+  }
+
+  public void setGender(char gender){
+    this.gender = gender;
+  }
+
+  public String getEmail(){
+    return email;
+  }
+
+  public void setEmail(String email){
+    this.email = email;
+  }
+
+  public String getPassword(){
+    return password;
+  }
+
+  public void setPassword(String password){
+    this.password = password;
+  }
 }
