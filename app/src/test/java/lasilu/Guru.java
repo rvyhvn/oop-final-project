198d70f35648b15da1e1e53d2b908865d8a16f12
rvyhvn <hanshitrash00@gmail.com>
2023-05-10 11:55:55 +0800

    Edit Class files on Test

--- app/src/test/java/lasilu/Guru.java
+++ app/src/test/java/lasilu/Guru.java
@@ -1,3 +1,16 @@
 package lasilu;
 
+/**
+ * Guru
+ */
+public class Guru extends User{
 
+  private int nip;
+  private MataPelajaran mataPelajaran;
+
+  public Guru(int id, String username, String nama, char gender, String email, String password, int nip, MataPelajaran mataPelajaran){
+    super(id, username, nama, gender, email, password);
+    this.nip = nip;
+    this.mataPelajaran = mataPelajaran;
+  }
+}
---
+++ app/src/test/java/lasilu/MataPelajaran.java
@@ -0,0 +1,15 @@
+package lasilu;
+
+/**
+ * MataPelajaran
+ */
+public class MataPelajaran {
+
+  private int idMapel;
+  private String namaMapel;
+
+  public MataPelajaran(int idMapel,  String namaMapel){
+    this.idMapel = idMapel;
+    this.namaMapel = namaMapel;
+  }
+}
