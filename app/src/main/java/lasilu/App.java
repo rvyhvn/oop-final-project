3b3811a6318b15847d9b8a7e0c73defb2f4bbf25
rvyhvn <hanshitrash00@gmail.com>
2023-05-16 23:48:53 +0800

    Adding some controllers, and testing postgreSQL. dont edit App.java file

--- app/src/main/java/lasilu/App.java
+++ app/src/main/java/lasilu/App.java
@@ -3,12 +3,23 @@
  */
 package lasilu;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         System.out.println(new App().getGreeting());
+        String JDBC_DRIVER = "org.postgresql.Driver";
+        Class.forName(JDBC_DRIVER);
+        String DB_URL = "jdbc:postgresql://localhost:5432/test";
+        String USER = "postgres";
+        String PASS = "";
+
+        Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
     }
 }
