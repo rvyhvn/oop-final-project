a4adcb94e2ca8684b231d0cbc832659737c0232c
rvyhvn <hanshitrash00@gmail.com>
2023-05-20 18:40:46 +0800

    Connecting from front-end to database

--- app/src/main/java/lasilu/App.java
+++ app/src/main/java/lasilu/App.java
@@ -23,7 +23,7 @@ public class App extends Application {
 
       Scene scene = new Scene(root);
       primaryStage.setScene(scene);
-      primaryStage.setTitle("Login FOrm");
+      primaryStage.setTitle("Login Form");
       primaryStage.show();
     }
     public String getGreeting() {
@@ -33,48 +33,6 @@ public class App extends Application {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         System.out.println(new App().getGreeting());
-        Properties properties = new Properties();
-        try (FileInputStream fis = new FileInputStream("/etc/postgresql/.config/oop-final-project/config.properties")) {
-            properties.load(fis);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-        String DB_URL = properties.getProperty("db.url");
-        String USER = properties.getProperty("db.username");
-        String PASS = properties.getProperty("db.password");
-
-        // Kode untuk memproses login
-        String username = "zakypeler"; // Ganti dengan username yang ingin Anda cek
-        String password = "pelerzaky"; // Ganti dengan password yang ingin Anda cek
-
-        Connection connection = null;
-        try {
-            // Membuat koneksi ke database
-            connection = DriverManager.getConnection(DB_URL, USER, PASS);
-
-            // Mengeksekusi query untuk memeriksa login
-            String query = "SELECT * FROM akun WHERE username = ? AND password = ?";
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setString(1, username);
-            statement.setString(2, password);
-
-            ResultSet resultSet = statement.executeQuery();
-
-            if (resultSet.next()) {
-                // Login berhasil
-                System.out.println("Login berhasil");
-            } else {
-                // Login gagal
-                System.out.println("Login gagal");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.close();
-            }
         launch(args);
         }
-    }
 }
--- app/src/main/java/lasilu/controllers/LoginController.java
+++ app/src/main/java/lasilu/controllers/LoginController.java
@@ -1,9 +1,19 @@
 package lasilu.controllers;
 
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.sql.DriverManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 
 public class LoginController {
     @FXML
@@ -20,12 +30,37 @@ public class LoginController {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
-        // Lakukan proses login sesuai kebutuhan Anda
-        // Contoh sederhana untuk memeriksa username dan password:
-        if (username.equals("admin") && password.equals("admin123")) {
+        Properties properties = new Properties();
+        try (FileInputStream fis = new FileInputStream("/etc/postgresql/.config/oop-final-project/config.properties")) {
+          properties.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+          }
+        String DB_URL = properties.getProperty("db.url");
+        String USER = properties.getProperty("db.username");
+        String PASS = properties.getProperty("db.password");
+
+        try {
+          Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+          String query = "SELECT * FROM akun WHERE username = ? AND password = ?";
+          PreparedStatement statement = connection.prepareStatement(query);
+          statement.setString(1, username);
+          statement.setString(2, password);
+
+          ResultSet resultSet = statement.executeQuery();
+          
+          if (resultSet.next()) {
             System.out.println("Login berhasil!");
-        } else {
+          } else {
             System.out.println("Login gagal!");
+          }
+          
+          connection.close();
+
+        } catch (SQLException e) {
+          e.printStackTrace();
         }
     }
+
 }
