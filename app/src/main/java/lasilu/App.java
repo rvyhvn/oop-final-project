3c6c610f2dcace515871b5035bf2b6f9ff189b35
princenizroh <princenizroh7@gmail.com>
2023-06-19 17:51:48 +0800

    add Login

--- app/src/main/java/lasilu/App.java
+++ app/src/main/java/lasilu/App.java
@@ -16,90 +16,50 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-// public class App extends Application {
+public class App extends Application {
 
-//     public static void main(String[] args) {
-//         launch(args);
-//     }
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-//     @Override
-//     public void start(Stage primaryStage) throws Exception {
-//         Connection connection = null;
-//         try {
-//             // Membuat koneksi ke database menggunakan DatabaseUtil
-//             connection = DatabaseUtil.getConnection();
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Connection connection = null;
+        try {
+            // Membuat koneksi ke database menggunakan DatabaseUtil
+            connection = DatabaseUtil.getConnection();
 
-//             // Load LoginView.fxml
-//             // Load file FXML untuk login view
-//             FXMLLoader loginLoader = new FXMLLoader(App.class.getResource("/lasilu/LoginView.fxml"));
-//             Parent loginRoot = loginLoader.load();
-//             LoginController loginController = loginLoader.getController();
-//             loginController.setConnection(connection);
-//             loginController.setOnLoginSuccess(() -> {
-//                 try {
-//                     // Load file FXML untuk main view
-//                     FXMLLoader mainLoader = new FXMLLoader(App.class.getResource("/lasilu/MainView.fxml"));
-//                     Parent mainRoot = mainLoader.load();
-//                     MainViewController mainController = mainLoader.getController();
-//                     mainController.initData(); // Mengambil data siswa dan melakukan inisialisasi tampilan
+            // Load LoginView.fxml
+            // Load file FXML untuk login view
+            FXMLLoader loginLoader = new FXMLLoader(App.class.getResource("/lasilu/view/LoginForm1.fxml"));
+            Parent loginRoot = loginLoader.load();
+            LoginController loginController = loginLoader.getController();
+            loginController.setConnection(connection);
+            loginController.setOnLoginSuccess(() -> {
+                try {
+                    // Load file FXML untuk main view
+                    FXMLLoader mainLoader = new FXMLLoader(App.class.getResource("../lasilu/view/MainView.fxml"));
+                    Parent mainRoot = mainLoader.load();
+                    MainViewController mainController = mainLoader.getController();
+                    mainController.initData(); // Mengambil data siswa dan melakukan inisialisasi tampilan
 
-//                     // Set scene untuk main view
-//                     primaryStage.setScene(new Scene(mainRoot));
-//                     primaryStage.show();
-//                 } catch (Exception e) {
-//                     e.printStackTrace();
-//                 }
-//             });
+                    // Set scene untuk main view
+                    primaryStage.setScene(new Scene(mainRoot));
+                    primaryStage.show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
 
-//             // Set scene untuk login view
-//             primaryStage.setScene(new Scene(loginRoot));
-//             primaryStage.show();
-//         } catch (Exception e) {
-//             e.printStackTrace();
-//         } finally {
-//             // Menutup koneksi dan sumber daya terkait menggunakan DatabaseUtil
-//             DatabaseUtil.closeConnection(connection);
-//         }
-//     }
-// }
-public class App {
-
-public static void main(String[] args) {
-    // Mendapatkan koneksi ke database
-    Connection connection = null;
-    try {
-        connection = DatabaseUtil.getConnection();
-
-        // Membuat objek SiswaController dengan koneksi yang diberikan
-        SiswaController siswaController = new SiswaController(connection);
-
-        List<Siswa> siswaList = siswaController.getAllSiswa();
-        System.out.println("Daftar Siswa:");
-        for (Siswa siswa : siswaList) {
-            System.out.println("ID: " + siswa.getIdSiswa());
-            System.out.println("Nama: " + siswa.getNama());
-            System.out.println("Email: " + siswa.getEmail());
-            System.out.println("Phone: " + siswa.getPhone());
-            System.out.println("Kelas ID: " + siswa.getKelas().getIdKelas());
-            System.out.println("Wali Murid ID: " + siswa.getWaliMurid().getIdWali());
-            System.out.println();
+            // Set scene untuk login view
+            primaryStage.setScene(new Scene(loginRoot));
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Menutup koneksi dan sumber daya terkait menggunakan DatabaseUtil
+            DatabaseUtil.closeConnection(connection);
         }
-
-        // Menggunakan fungsi getSiswaById
-        int idKelas = 1;
-        List<Siswa> siswaByKelas = siswaController.getSiswaByKelasId(idKelas);
-        System.out.println("Daftar Siswa dengan Kelas ID " + idKelas + ":");
-        for (Siswa siswa : siswaByKelas) {
-            System.out.println("ID: " + siswa.getIdSiswa());
-            System.out.println("Nama: " + siswa.getNama());
-            System.out.println("Email: " + siswa.getEmail());
-            System.out.println("Phone: " + siswa.getPhone());
-            System.out.println("Kelas ID: " + siswa.getKelas().getIdKelas());
-            System.out.println("Wali Murid ID: " + siswa.getWaliMurid().getIdWali());
-            System.out.println();
-        }
-    } catch (SQLException e) {
-        e.printStackTrace();
-    } 
-  }
+    }
 }
+
