17011bebdc864d1a08bd108b7a44e002f4f80459
princenizroh <princenizroh7@gmail.com>
2023-06-19 14:39:01 +0800

    Fixed directory and add sendMassageBox.fxml

--- app/src/main/java/lasilu/App.java
+++ app/src/main/java/lasilu/App.java
@@ -16,81 +16,47 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class App extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Connection connection = null;
-        try {
-            // Membuat koneksi ke database menggunakan DatabaseUtil
-            connection = DatabaseUtil.getConnection();
-
-            // Load LoginView.fxml
-            // Load file FXML untuk login view
-            FXMLLoader loginLoader = new FXMLLoader(App.class.getResource("/lasilu/LoginView.fxml"));
-            Parent loginRoot = loginLoader.load();
-            LoginController loginController = loginLoader.getController();
-            loginController.setConnection(connection);
-            loginController.setOnLoginSuccess(() -> {
-                try {
-                    // Load file FXML untuk main view
-                    FXMLLoader mainLoader = new FXMLLoader(getClass().getResource("/lasilu/MainView.fxml"));
-                    Parent mainRoot = mainLoader.load();
-                    MainViewController mainController = mainLoader.getController();
-                    mainController.initData(); // Mengambil data siswa dan melakukan inisialisasi tampilan
-
-                    // Set scene untuk main view
-                    primaryStage.setScene(new Scene(mainRoot));
-                    primaryStage.show();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-
-            // Set scene untuk login view
-            primaryStage.setScene(new Scene(loginRoot));
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // Menutup koneksi dan sumber daya terkait menggunakan DatabaseUtil
-            DatabaseUtil.closeConnection(connection);
-        }
-    }
-}
-// public class App {
+// public class App extends Application {
 // public static void main(String[] args) {
+// launch(args);
+// }
+
+// @Override
+// public void start(Stage primaryStage) throws Exception {
 // Connection connection = null;
 // try {
 // // Membuat koneksi ke database menggunakan DatabaseUtil
 // connection = DatabaseUtil.getConnection();
 
-// // Membuat objek SiswaController
-// SiswaController siswaController = new SiswaController(connection);
+// // Load LoginView.fxml
+// // Load file FXML untuk login view
+// FXMLLoader loginLoader = new
+// FXMLLoader(App.class.getResource("/lasilu/LoginView.fxml"));
+// Parent loginRoot = loginLoader.load();
+// LoginController loginController = loginLoader.getController();
+// loginController.setConnection(connection);
+// loginController.setOnLoginSuccess(() -> {
+// try {
+// // Load file FXML untuk main view
+// FXMLLoader mainLoader = new
+// FXMLLoader(getClass().getResource("/lasilu/MainView.fxml"));
+// Parent mainRoot = mainLoader.load();
+// MainViewController mainController = mainLoader.getController();
+// mainController.initData(); // Mengambil data siswa dan melakukan inisialisasi
+// tampilan
 
-// // Mengambil semua siswa dari database
-// List<Siswa> siswaList = siswaController.getAllSiswa();
-
-// // Menampilkan informasi siswa
-// for (Siswa siswa : siswaList) {
-// System.out.println("ID Siswa: " + siswa.getIdSiswa());
-// System.out.println("Nama: " + siswa.getNama());
-// System.out.println("Email: " + siswa.getEmail());
-// System.out.println("Phone: " + siswa.getPhone());
-// System.out.println("ID Kelas: " + siswa.getKelas().getIdKelas());
-// System.out.println("Tingkat Kelas: " + siswa.getKelas().getTingkat());
-// System.out.println("Urutan Kelas: " + siswa.getKelas().getUrutan());
-// System.out.println("Is IPA: " + siswa.getKelas().getIsIpa());
-// System.out.println("ID Wali Murid: " + siswa.getWaliMurid().getIdWali());
-// System.out.println("Nama Wali Murid: " + siswa.getWaliMurid().getNama());
-// System.out.println("Email Wali Murid: " + siswa.getWaliMurid().getEmail());
-// System.out.println("Phone Wali Murid: " + siswa.getWaliMurid().getPhone());
-// System.out.println("----------------------------------");
+// // Set scene untuk main view
+// primaryStage.setScene(new Scene(mainRoot));
+// primaryStage.show();
+// } catch (Exception e) {
+// e.printStackTrace();
 // }
-// } catch (SQLException e) {
+// });
+
+// // Set scene untuk login view
+// primaryStage.setScene(new Scene(loginRoot));
+// primaryStage.show();
+// } catch (Exception e) {
 // e.printStackTrace();
 // } finally {
 // // Menutup koneksi dan sumber daya terkait menggunakan DatabaseUtil
@@ -98,3 +64,40 @@ public class App extends Application {
 // }
 // }
 // }
+public class App {
+    public static void main(String[] args) {
+        Connection connection = null;
+        try {
+            // Membuat koneksi ke database menggunakan DatabaseUtil
+            connection = DatabaseUtil.getConnection();
+
+            // Membuat objek SiswaController
+            SiswaController siswaController = new SiswaController(connection);
+
+            // Mengambil semua siswa dari database
+            List<Siswa> siswaList = siswaController.getAllSiswa();
+
+            // Menampilkan informasi siswa
+            for (Siswa siswa : siswaList) {
+                System.out.println("ID Siswa: " + siswa.getIdSiswa());
+                System.out.println("Nama: " + siswa.getNama());
+                System.out.println("Email: " + siswa.getEmail());
+                System.out.println("Phone: " + siswa.getPhone());
+                System.out.println("ID Kelas: " + siswa.getKelas().getIdKelas());
+                System.out.println("Tingkat Kelas: " + siswa.getKelas().getTingkat());
+                System.out.println("Urutan Kelas: " + siswa.getKelas().getUrutan());
+                System.out.println("Is IPA: " + siswa.getKelas().getIsIpa());
+                System.out.println("ID Wali Murid: " + siswa.getWaliMurid().getIdWali());
+                System.out.println("Nama Wali Murid: " + siswa.getWaliMurid().getNama());
+                System.out.println("Email Wali Murid: " + siswa.getWaliMurid().getEmail());
+                System.out.println("Phone Wali Murid: " + siswa.getWaliMurid().getPhone());
+                System.out.println("----------------------------------");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Menutup koneksi dan sumber daya terkait menggunakan DatabaseUtil
+            DatabaseUtil.closeConnection(connection);
+        }
+    }
+}
