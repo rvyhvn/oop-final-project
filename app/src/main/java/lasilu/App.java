24e72aa9a3308f84c48400bd25cda2b34d249b75
febrynn <11221008@student.itk.ac.id>
2023-06-20 07:22:26 -0700

    fix at App.java

--- app/src/main/java/lasilu/App.java
+++ app/src/main/java/lasilu/App.java
@@ -19,58 +19,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /*
- * This Dashboard class
- */
-// public class App extends Application {
-
-//     public void showSendMessageBox(){
-        
-//         try {
-//             FXMLLoader msgLoader = new FXMLLoader(App.class.getResource("/lasilu/view/SendMessageBox.fxml"));
-//             Parent msgRoot = msgLoader.load();
-//             // set Scene for the message
-//             Stage stage = new Stage();
-//             stage.setTitle("Send Message");
-            
-//             stage.setScene(new Scene(msgRoot));
-//             stage.resizableProperty().setValue(false);
-//             stage.show();
-//         } catch (IOException e) {
-//             e.printStackTrace();
-//         }
-//     }
-
-//     public static void main(String[] args) {
-//         launch(args);
-//     }
-
-//     @Override
-//     public void start(Stage primaryStage) throws Exception {
-//         Connection connection = null;
-//         try {
-//             // Membuat koneksi ke database menggunakan DatabaseUtil
-//             connection = DatabaseUtil.getConnection();
-
-//             // Load Dashboard.fxml
-//             // Load file FXML untuk Dashboard
-//             FXMLLoader loginLoader = new FXMLLoader(App.class.getResource("/lasilu/view/Dashboard.fxml"));
-//             Parent loginRoot = loginLoader.load();
-//             // Set scene untuk login view
-//             DashboardController2 dashboardController = loginLoader.getController();
-//             dashboardController.setApp(this);
-//             primaryStage.setScene(new Scene(loginRoot));
-//             primaryStage.resizableProperty().setValue(false);
-//             primaryStage.show();
-//         } catch (Exception e) {
-//             e.printStackTrace();
-//         } finally {
-//             // Menutup koneksi dan sumber daya terkait menggunakan DatabaseUtil
-//             DatabaseUtil.closeConnection(connection);
-//         }
-//     }
-// }
-
-/*
  * This class loginForm
  */
 
