1c8109c2ba7f09759b064a7620b40d9ee84ac52e
rvyhvn <hanshitrash00@gmail.com>
2023-06-20 16:44:55 +0800

    resolve: conflict SendMessage and App

--- app/src/main/java/lasilu/App.java
+++ app/src/main/java/lasilu/App.java
@@ -16,43 +16,159 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+// /*
+//  * This class Email
+//  */
+// public class App extends Application {
 
-public class App {
+//     public static void main(String[] args) {
+//         launch(args);
+//     }
+
+//     @Override
+//     public void start(Stage primaryStage) throws Exception {
+//         Connection connection = null;
+//         try {
+//             // Membuat koneksi ke database menggunakan DatabaseUtil
+//             connection = DatabaseUtil.getConnection();
+
+//             // Load sendMassageBox.fxml
+//             // Load file FXML untuk sendMassageBox
+//             FXMLLoader loginLoader = new FXMLLoader(App.class.getResource("/lasilu/view/sendMassageBox.fxml"));
+//             Parent loginRoot = loginLoader.load();
+//             // Set scene untuk login view
+//             primaryStage.setScene(new Scene(loginRoot));
+//             primaryStage.resizableProperty().setValue(false);
+//             primaryStage.show();
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         } finally {
+//             // Menutup koneksi dan sumber daya terkait menggunakan DatabaseUtil
+//             DatabaseUtil.closeConnection(connection);
+//         }
+//     }
+// }
+
+/*
+ * This class Dashboard
+ */
+public class App extends Application {
 
     public static void main(String[] args) {
-        // Mendapatkan koneksi ke database
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
         Connection connection = null;
         try {
+            // Membuat koneksi ke database menggunakan DatabaseUtil
             connection = DatabaseUtil.getConnection();
 
-            SiswaController siswaController = new SiswaController(connection);
-            int idSiswa = 11;
-            String nama = "Zaky Dio";
-            String email = "zakydio@example.com";
-            String phone = "1234567890";
-            int idKelas = 1;
-            int idWali = 1;
-            int idNilai = 1;
-
-            // Menjalankan fungsi updateSiswa
-            siswaController.updateSiswa(idSiswa, nama, email, phone, idWali, idKelas, idNilai);
-
-            List<Siswa> siswaList = siswaController.getAllSiswa();
-            System.out.println("Daftar Siswa setelah pembaruan:");
-            for (Siswa siswa : siswaList) {
-                System.out.println("ID: " + siswa.getIdSiswa());
-                System.out.println("Nama: " + siswa.getNama());
-                System.out.println("Email: " + siswa.getEmail());
-                System.out.println("Phone: " + siswa.getPhone());
-                System.out.println("Kelas ID: " + siswa.getKelas().getIdKelas());
-                System.out.println("Wali Murid ID: " + siswa.getWaliMurid().getIdWali());
-                System.out.println();
-            }
-        } catch (SQLException e) {
+            // Load Dashboard.fxml
+            // Load file FXML untuk Dashboard
+            FXMLLoader loginLoader = new FXMLLoader(App.class.getResource("/lasilu/view/Dashboard.fxml"));
+            Parent loginRoot = loginLoader.load();
+            // Set scene untuk login view
+            primaryStage.setScene(new Scene(loginRoot));
+            primaryStage.resizableProperty().setValue(false);
+            primaryStage.show();
+        } catch (Exception e) {
             e.printStackTrace();
-            // Menutup koneksi database
+        } finally {
+            // Menutup koneksi dan sumber daya terkait menggunakan DatabaseUtil
+            DatabaseUtil.closeConnection(connection);
         }
     }
 }
+/*
+ * This class loginForm
+ */
 
+
+// <<<<<<< HEAD
+// public class App {
+
+//     public static void main(String[] args) {
+//         // Mendapatkan koneksi ke database
+//         Connection connection = null;
+//         try {
+//             connection = DatabaseUtil.getConnection();
+
+//             SiswaController siswaController = new SiswaController(connection);
+//             int idSiswa = 11;
+//             String nama = "Zaky Dio";
+//             String email = "zakydio@example.com";
+//             String phone = "1234567890";
+//             int idKelas = 1;
+//             int idWali = 1;
+//             int idNilai = 1;
+
+//             // Menjalankan fungsi updateSiswa
+//             siswaController.updateSiswa(idSiswa, nama, email, phone, idWali, idKelas, idNilai);
+
+//             List<Siswa> siswaList = siswaController.getAllSiswa();
+//             System.out.println("Daftar Siswa setelah pembaruan:");
+//             for (Siswa siswa : siswaList) {
+//                 System.out.println("ID: " + siswa.getIdSiswa());
+//                 System.out.println("Nama: " + siswa.getNama());
+//                 System.out.println("Email: " + siswa.getEmail());
+//                 System.out.println("Phone: " + siswa.getPhone());
+//                 System.out.println("Kelas ID: " + siswa.getKelas().getIdKelas());
+//                 System.out.println("Wali Murid ID: " + siswa.getWaliMurid().getIdWali());
+//                 System.out.println();
+//             }
+//         } catch (SQLException e) {
+//             e.printStackTrace();
+//             // Menutup koneksi database
+//         }
+//     }
+// }
+
+// =======
+//     public static void main(String[] args) {
+//         launch(args);
+//     }
+
+//     @Override
+//     public void start(Stage primaryStage) throws Exception {
+//         Connection connection = null;
+//         try {
+//             // Membuat koneksi ke database menggunakan DatabaseUtil
+//             connection = DatabaseUtil.getConnection();
+
+//             // Load LoginForm.fxml
+//             // Load file FXML untuk login Form
+//             FXMLLoader loginLoader = new FXMLLoader(App.class.getResource("/lasilu/view/LoginForm1.fxml"));
+//             Parent loginRoot = loginLoader.load();
+//             LoginController loginController = loginLoader.getController();
+//             loginController.setConnection(connection);
+//             loginController.setOnLoginSuccess(() -> {
+//                 try {
+//                     // Load file FXML untuk main view
+//                     FXMLLoader mainLoader = new FXMLLoader(App.class.getResource("../lasilu/view/MainView.fxml"));
+//                     Parent mainRoot = mainLoader.load();
+//                     MainViewController mainController = mainLoader.getController();
+//                     mainController.initData(); // Mengambil data siswa dan melakukan inisialisasi tampilan
+
+//                     // Set scene untuk main view
+//                     primaryStage.setScene(new Scene(mainRoot));
+//                     primaryStage.show();
+//                 } catch (Exception e) {
+//                     e.printStackTrace();
+//                 }
+//             });
+
+//             // Set scene untuk login view
+//             primaryStage.setScene(new Scene(loginRoot));
+//             primaryStage.show();
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         } finally {
+//             // Menutup koneksi dan sumber daya terkait menggunakan DatabaseUtil
+//             DatabaseUtil.closeConnection(connection);
+//         }
+//     }
+// }
---
+++ app/src/main/java/lasilu/controller/DashboardController.java
@@ -0,0 +1,210 @@
+package lasilu.controller;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.util.Callback;
+import lasilu.dao.SiswaDAO;
+import lasilu.model.Siswa;
+import lasilu.util.DatabaseUtil;
+import javafx.geometry.Pos;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+    @FXML
+    private AnchorPane rootPane;
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    private TableView<DataItem> tableView;
+
+    @FXML
+    private TableColumn<DataItem, Integer> noColumn;
+
+    @FXML
+    private TableColumn<DataItem, String> namaColumn;
+
+    @FXML
+    private TableColumn<DataItem, String> waliMuridColumn;
+
+    @FXML
+    private TableColumn<DataItem, Double> nilaiRataRataColumn;
+
+    @FXML
+    private HBox comboBoxContainer;
+
+    @FXML
+    private ComboBox<String> kelasComboBox;
+
+    @FXML
+    private AnchorPane logoPane;
+
+    @FXML
+    private VBox logoContainer;
+
+    @FXML
+    private ImageView logoImageView;
+
+    @FXML
+    private Button buatLaporanButton;
+
+    private SiswaDAO siswaDAO;
+
+    private SiswaController siswaController;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            Connection connection = DatabaseUtil.getConnection(); // Menghubungkan ke database
+            siswaController = new SiswaController(connection);
+            setupTableColumns();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Handle error connecting to the database
+        }
+        loadSiswaData();
+        setupTableColumns();
+        setupComboBox();
+
+    }
+
+    private void loadSiswaData() {
+        // Ambil data siswa dari objek siswaDAO
+        List<Siswa> daftarSiswa = siswaController.getAllSiswa();
+
+        // Buat daftar item data
+        List<DataItem> daftarData = new ArrayList<>();
+        int no = 1;
+
+        for (Siswa siswa : daftarSiswa) {
+            // Ambil nama siswa
+            String namaSiswa = siswa.getNama();
+
+            // Buat item data baru dengan menggunakan nama siswa
+            DataItem dataItem = new DataItem(no, namaSiswa, "asda", 0.0);
+            daftarData.add(dataItem);
+
+            no++;
+        }
+
+        // Set data ke dalam tabel
+        tableView.getItems().setAll(daftarData);
+    }
+
+    private void setupTableColumns() {
+        noColumn.setCellValueFactory(cellData -> cellData.getValue().idProperty().asObject());
+        namaColumn.setCellValueFactory(cellData -> cellData.getValue().namaProperty());
+        waliMuridColumn.setCellValueFactory(cellData -> cellData.getValue().waliMuridProperty());
+        nilaiRataRataColumn.setCellValueFactory(cellData -> cellData.getValue().nilaiRataRataProperty().asObject());
+        // Set table data
+        // Buat ObservableList kosong untuk menginisialisasi tableView
+        ObservableList<DataItem> dataItems = FXCollections.observableArrayList();
+
+        // Set data ke dalam tabel
+        tableView.setItems(dataItems);
+    }
+
+    private void setupComboBox() {
+        // Setup combo box items and event handler
+        kelasComboBox.setItems(null); // Ganti dengan data yang sesuai
+        kelasComboBox.setOnAction(event -> {
+            // Perform action when combo box selection changes
+        });
+    }
+
+    @FXML
+    private void buatLaporan() {
+        // Implementasi logika untuk membuat laporan di sini
+    }
+
+    // Data model class
+    public static class DataItem {
+        private final IntegerProperty id = new SimpleIntegerProperty();
+        private final StringProperty nama = new SimpleStringProperty();
+        private final StringProperty waliMurid = new SimpleStringProperty();
+        private final DoubleProperty nilaiRataRata = new SimpleDoubleProperty();
+
+        public DataItem(int id, String nama, String waliMurid, double nilaiRataRata) {
+            setId(id);
+            setNama(nama);
+            setWaliMurid(waliMurid);
+            setNilaiRataRata(nilaiRataRata);
+        }
+
+        public int getId() {
+            return id.get();
+        }
+
+        public IntegerProperty idProperty() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id.set(id);
+        }
+
+        public String getNama() {
+            return nama.get();
+        }
+
+        public StringProperty namaProperty() {
+            return nama;
+        }
+
+        public void setNama(String nama) {
+            this.nama.set(nama);
+        }
+
+        public String getWaliMurid() {
+            return waliMurid.get();
+        }
+
+        public StringProperty waliMuridProperty() {
+            return waliMurid;
+        }
+
+        public void setWaliMurid(String waliMurid) {
+            this.waliMurid.set(waliMurid);
+        }
+
+        public double getNilaiRataRata() {
+            return nilaiRataRata.get();
+        }
+
+        public DoubleProperty nilaiRataRataProperty() {
+            return nilaiRataRata;
+        }
+
+        public void setNilaiRataRata(double nilaiRataRata) {
+            this.nilaiRataRata.set(nilaiRataRata);
+        }
+    }
+}
